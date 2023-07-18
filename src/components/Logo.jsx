28c0de8a92a63c622f30d125cc7296a1a9df45de
albertoflorence <alberto.sabatier@gmail.com>
2023-07-18 16:54:22 -0300

    feat: Logo redireciona o usuário para página de meals

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import logoFull from '../images/logo-full.png';
 import logo from '../images/logo.png';
@@ -7,10 +8,10 @@ import logoText from '../images/logo-text.png';
 
 export default function Logo({ full = false, text = false }) {
   return (
-    <div className="logo">
+    <Link className="logo" to="/meals">
       <img src={ full ? logoFull : logo } alt="logo" />
       {text && <img src={ logoText } alt="recipes app" className="mx-3" />}
-    </div>
+    </Link>
   );
 }
 
